5ae3dc67d39d3426122ff9303096f142273701c9
moosterhuis <michiel.oosterhuis@capgemini.com>
2024-04-04 12:09:57 +0200

    Refactor

--- ChrystalBalls.java
+++ ChrystalBalls.java
@@ -1,27 +1,27 @@
 public class ChrystalBalls {
-	
+
 	public static void main(String[] args) {
-		
-		boolean[] breaks = new boolean[]{false, true, true, true, true, true, true, true};
+
+		boolean[] breaks = new boolean[] { false, true, true, true, true, true, true, true };
 		System.out.println(search(breaks));
 	}
-	
+
 	public static int search(boolean[] breaks) {
 		int subHeight = (int) Math.sqrt(breaks.length);
 		int i = subHeight;
-		for(;i < breaks.length; i += subHeight) {
-			if(breaks[i]) {
+		for (; i < breaks.length; i += subHeight) {
+			if (breaks[i]) {
 				break;
 			}
 		}
-		
+
 		i -= subHeight;
-		for(int j = 0; j <= subHeight && i < breaks.length; i++, j++) {
-			if(breaks[i]) {
+		for (int j = 0; j <= subHeight && i < breaks.length; i++, j++) {
+			if (breaks[i]) {
 				return i;
 			}
 		}
 		return -1;
-		
+
 	}
 }

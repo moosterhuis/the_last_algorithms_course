648e570cb60f53ca18fc32dd25b6c077411e5989
Michiel Oosterhuis <michiel.oosterhuis@gmail.com>
2024-04-10 09:11:25 +0200

    Update

--- WeightedAdjacencyList.java
+++ WeightedAdjacencyList.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class Edge {
-	int to;
-	int weigth;
+	public int to;
+	public int weigth;
 
 	public Edge(int to, int weigth) {
 		this.to = to;
@@ -16,12 +17,45 @@ public class WeightedAdjacencyList {
 	public static void main(String[] args) {
 		Edge[][] graph = {
 				{ new Edge(1, 1), new Edge(2, 4), new Edge(3, 5) },
-				{ new Edge(0, 1) },
-				{ new Edge(2, 1) },
+				{},
+				{ new Edge(3, 2) },
+				{ new Edge(4, 5) },
+				{}
 		};
+		System.out.println(depthFirstSearch(graph, 0, 2));
 	}
 
-	public List<Integer> depthFirstSearch(Edge[][] graph, int source, int needle) {
-		return null;
+	public static List<Integer> depthFirstSearch(Edge[][] graph, int source, int needle) {
+		boolean[] seen = new boolean[graph.length];
+		for (int i = 0; i < seen.length; i++) {
+			seen[i] = false;
+		}
+		List<Integer> path = new ArrayList<>();
+		walk(graph, source, needle, path, seen);
+		return path;
+	}
+
+	public static boolean walk(Edge[][] graph, int curr, int needle, List<Integer> path, boolean[] seen) {
+
+		if (seen[curr]) {
+			return false;
+		}
+		seen[curr] = true;
+
+		// pre
+		if (curr == needle) {
+			path.add(curr);
+			return true;
+		}
+		// recurse
+		Edge[] list = graph[curr];
+		for (Edge edge : list) {
+			if (walk(graph, edge.to, needle, path, seen)) {
+				path.add(0, curr);
+				return true;
+			}
+		}
+		// post
+		return false;
 	}
 }
